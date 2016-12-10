da576e03f86938e581fd4d404deabf1489759e77
Vojislav Grujic <vojislav.grujic@gmail.com>
2016-12-10 05:13:54 +0100

    Initial commit. Everyone on board!

---
+++ script.js
@@ -0,0 +1,39 @@
+window,addEventListener('load', function(){
+  var button = document.querySelector('.start'),
+      minutes = document.querySelector('.minutes'),
+      seconds = document.querySelector('.seconds'),
+      timeOver = document.querySelector('#timeout'),
+      puzzle = document.querySelector('.puzzle'),
+      solved = document.querySelector('.solved'),
+      timer;
+
+  button.addEventListener('click', function() {
+    startTimer();
+  })
+
+  function startTimer() {
+    timer = window.setTimeout(function(){
+      seconds.textContent = +seconds.textContent - 1;
+      window.clearTimeout(timer);
+      if ( minutes.textContent == 0 && seconds.textContent == 0 ) {
+        // code to run after the time has ran out
+        timeOver.play();
+        return;
+      }
+      if ( minutes.textContent != 0 && seconds.textContent == 0 ) {
+        minutes.textContent = +minutes.textContent - 1;
+        seconds.textContent = 59;
+      }
+      startTimer();
+    }, 10);
+  }
+
+  puzzle.addEventListener('mousedown', function() {
+    solved.style.opacity = 0.8; 
+  })
+
+  puzzle.addEventListener('mouseup', function() {
+    solved.style.opacity = 0; 
+  })
+
+});
